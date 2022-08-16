4c87dd15be53cd5e27915f29af4ec162c2711cbf
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-17 00:15:47 +0100

    added services package

---
+++ src/main/java/com/example/johnny/microservice02/services/TourService.java
@@ -0,0 +1,55 @@
+package com.example.johnny.microservice02.services;
+
+import com.example.johnny.microservice02.domain.Difficulty;
+import com.example.johnny.microservice02.domain.Region;
+import com.example.johnny.microservice02.domain.Tour;
+import com.example.johnny.microservice02.domain.TourPackage;
+import com.example.johnny.microservice02.repositories.TourPackageRepository;
+import com.example.johnny.microservice02.repositories.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourService {
+    private final TourRepository tourRepository;
+    private final TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a new Tour Object and persist it to the Database.
+     *
+     * @param title title
+     * @param description description
+     * @param blurb blurb
+     * @param price price
+     * @param duration duration
+     * @param bullets comma-separated bullets
+     * @param keywords keywords
+     * @param tourPackageName tour package name
+     * @param difficulty difficulty
+     * @param region region
+     * @return Tour Entity
+     */
+    public Tour createTour(String title, String description, String blurb, Integer price,
+                           String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
+        TourPackage tourPackage = null;
+
+        return new Tour(title, description,blurb, price, duration, bullets, keywords, tourPackage, difficulty, region);
+    }
+
+    /**
+     * Calculate the number of Tours in the Database.
+     *
+     * @return the total.
+     */
+    public long total() {
+        return 0;
+    }
+
+}
