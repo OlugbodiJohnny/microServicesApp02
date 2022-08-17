9ddfd550d7d7017ab2f13f5c19b9590286b0dd3e
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-17 02:19:45 +0100

    added HAL Browser Integration

--- src/main/java/com/example/johnny/microservice02/repositories/TourRepository.java
+++ src/main/java/com/example/johnny/microservice02/repositories/TourRepository.java
@@ -3,11 +3,10 @@ package com.example.johnny.microservice02.repositories;
 import com.example.johnny.microservice02.domain.Tour;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
 
 /**
  * Tour Repository Interface
@@ -22,4 +21,29 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Integer
      * @return List of found tours.
      */
     Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
+
