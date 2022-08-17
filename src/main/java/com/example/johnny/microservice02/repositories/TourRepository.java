15404d1ede0729008c3d30db5d156254177f45e3
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-17 01:56:34 +0100

    added pagination to findByTourPackage method

--- src/main/java/com/example/johnny/microservice02/repositories/TourRepository.java
+++ src/main/java/com/example/johnny/microservice02/repositories/TourRepository.java
@@ -1,7 +1,25 @@
 package com.example.johnny.microservice02.repositories;
 
 import com.example.johnny.microservice02.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+import java.util.List;
+
+/**
+ * Tour Repository Interface
+ *
+ * Created by Mary Ellen Bowman
+ */
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    /**
+     * Find Tours associated with the Tour Package.
+     *
+     * @param code tour package code
+     * @return List of found tours.
+     */
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
 }
